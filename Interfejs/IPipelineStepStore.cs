97cfbbe02b8b5e1afbe930daf2392a702aa3392a
Piotr <Piotr@DESKTOP-3MB7268>
2026-02-19 23:58:32 +0100

    as

--- Interfejs/IPipelineStepStore.cs
+++ Interfejs/IPipelineStepStore.cs
@@ -1,6 +1,9 @@
-﻿public interface IPipelineStepStore
+﻿namespace STSAnaliza.Interfejs;
+
+public interface IPipelineStepStore
 {
+    string FilePath { get; }
+
     Task<List<StepDefinition>> LoadAsync(CancellationToken ct = default);
     Task SaveAsync(List<StepDefinition> steps, CancellationToken ct = default);
-    string FilePath { get; }
 }
--- OptionsForm.cs
+++ OptionsForm.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using STSAnaliza.Interfejs;
 
 namespace STSAnaliza
 {
